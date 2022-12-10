0ef7662cdaf394239a75fcdd5ebaae322fe722cd
fuzongjing <202003150904@zjut.edu.cn>
2022-12-10 20:07:05 +0800

    修改timer_sleep

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -119,6 +119,7 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 
 void thread_block (void);
 void thread_unblock (struct thread *);
+void sleep_time_update(struct thread* t, void* aux UNUSED);
 
 struct thread *thread_current (void);
 tid_t thread_tid (void);
